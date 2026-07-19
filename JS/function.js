05faba29c6e215dbfc6fdf0fc90608fdd5e1f826
Omar-Javeid <omarjaveidd@gmail.com>
2026-07-19 14:12:55 +0200

    update repo

--- JS/function.js
+++ JS/function.js
@@ -1,25 +1,28 @@
 let numbers = ''
 let numArray = []
 let numArrayInt = []
+let calculationArray = []
 let display = '';
-let calculateArr = []
+let operationArray = []
 let bracketCount = 0;
-let result = 0;
+let result = 0.0;
+let queue = []
+let finalresult = 0;
 
 function addNum(num) {
     display += num;
     numbers += num
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function backspace() {
     display = display.slice(0, -1);
     display = display.trimEnd();
-    calculateArr.pop()
+    operationArray.pop()
     numArray.pop()
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function clearAll() {
@@ -27,171 +30,138 @@ function clearAll() {
     numArray = []
     numArrayInt = []
     display = '';
-    calculateArr = []
+    operationArray = []
     bracketCount = 0;
     result = 0;
+    queue = []
+    calculationArray = []
     document.getElementById('inputBox').innerHTML = ''
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function Add() {
     numArray.push(numbers)
     numbers = '';
     display += ' + '
-    calculateArr.push('+')
+    operationArray.push('+')
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function Subtract() {
     numArray.push(numbers)
     numbers = '';
     display += ' - '
-    calculateArr.push('-')
+    operationArray.push('-')
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function Percent() {
     numArray.push(numbers)
     numbers = '';
     display += '% '
-    calculateArr.push('%')
+    operationArray.push('%')
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function Divide() {
     numArray.push(numbers)
     numbers = '';
     display += ' ÷ '
-    calculateArr.push('/')
+    operationArray.push('/')
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function Multiply() {
     numArray.push(numbers)
     numbers = '';
     display += ' x '
-    calculateArr.push('*')
+    operationArray.push('*')
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display,'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display,'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
 
 function Brackets() {
     bracketCount++
     if (bracketCount % 2) {
         display += '('
-        calculateArr.push('(')
+        operationArray.push('(')
     } else {
         display += ')'
-        calculateArr.push(')')
+        operationArray.push(')')
+        numArray.push(numbers)
     }
     document.getElementById('inputBox').innerHTML = display
-    console.log('display: ' + display, 'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray)
+    console.log('display: ' + display, 'calculation array: ' + operationArray, 'numbers: ' + numbers, 'numArray:  ' + numArray)
 }
-
 
 function Calculate() {
     numArray.push(numbers)
-    
+    calculationArray = queue.toSorted()
     for (let i = 0; i < numArray.length; i++) {
         numArrayInt.push(parseFloat(numArray[i]))
     }
 
-    for (let i = 0; i < calculateArr.length; i++) {
+    for (let i = 0; i < numArrayInt.length; i++) {
+        if (calculationArray[i] == 'a') {
 
-        if (calculateArr[i] == '(') {
+        } else if (calculationArray[i] == 'ba') {
+            result = numArrayInt[operationArray.indexOf('*')] * numArrayInt[calculationArray.indexOf('*') + 1]
+            finalresult /= result
+        } else if (calculationArray[i] == 'bb') {
+            result = numArrayInt[operationArray.indexOf('/')] / numArrayInt[operationArray.indexOf('/') + 1]
+            finalresult /= result
 
-        }
+        } else if (calculationArray[i] == 'ca') {
+            result = numArrayInt[operationArray.indexOf('%')]
+            finalresult %= result
 
-        if (calculateArr[i] == '%') {
-            if (calculateArr[i] == calculateArr[i - 1]) {
-                result %= numArrayInt[i]
-                numArrayInt.splice(i-1,1,result)
-            } else {
-                result = numArrayInt[i] % numArrayInt[i + 1]
-                numArrayInt.splice(i,1,result)
-            }
-            result %= result
-        }
-
-        if (calculateArr[i] == '*') {
-            result = 1
-            if (calculateArr[i] == calculateArr[i - 1]) {
-                result *= numArrayInt[i]
-                numArrayInt.splice(i-1,1,result)
-            } else {
-                result = numArrayInt[i] * numArrayInt[i + 1]
-                numArrayInt.splice(i,1,result)
-            }
-            result = result
-        }
-
-
-
-        if (calculateArr[i] == '/') {
-            result = 1
-            if (calculateArr[i] == calculateArr[i - 1]) {
-                result /= numArrayInt[i]
-                numArrayInt.splice(i-1,1,result)
-            } else {
-                result = numArrayInt[i] / numArrayInt[i+1]
-                numArrayInt.splice(i,1,result)
-            }
-            result /= result
-        }
-
-
-        if (calculateArr[i] == '+') {
-            if (calculateArr[i] == calculateArr[i - 1]) {
+        } else if (operationArray[i] == '+') {
+            if (operationArray.length > 1) {
                 result += numArrayInt[i]
-                numArrayInt.splice(i-1,1,result)
             } else {
                 result = numArrayInt[i] + numArrayInt[i + 1]
-                    .splice(i,1,result)
             }
-            result += result
-        }
-
-
-        if (calculateArr[i] == '-') {
-            if (calculateArr[i] == calculateArr[i - 1]) {
-                result -= numArrayInt[i]
-                numArrayInt.splice(i-1,1,result)
-            } else {
+        } else if (operationArray[i] === '-') {
+            if (operationArray.length > 1) {
                 result = numArrayInt[i] - numArrayInt[i + 1]
-                numArrayInt.splice(i,1,result)
+                numArrayInt.slice(i)
+            } else {
+                result -= numArrayInt[i]
             }
-            result = result
+
         }
-        console.log(numArrayInt[i] + ' ' + calculateArr[i] + ' ' + numArrayInt[i+1] + ' = ' + result, i, numArrayInt)
-        
-    }
-    document.getElementById('inputBox').innerHTML = '= ' + numArrayInt[0]
-    console.log('display: ' + display, 'calculation array: ' + calculateArr, 'numbers: ' + numbers, 'numArray:  ' + numArray, 'Result: ' + result)
-}
-let order = [];
-function Pemdas() {
-    for (let i = 0; i < calculateArr.length; i++) {
-        if (calculateArr[i] == '(') {
-            order.push('(')
-        }
-        if (calculateArr[i] == '*') {
-            order.push('*')
-        }
-        if (calculateArr[i] == '/') {
-            order.push('/')
-        }
-        if (calculateArr[i] == '%') {
-            order.push('%')
-        }
-        if (calculateArr[i] == '+') {
-            order.push('+')
-        }
-        if (calculateArr[i] == '-') {
-            order.push('-')
-        }
+
+        document.getElementById('inputBox').innerHTML = result
+        console.log(result, numArrayInt, operationArray, calculationArray)
+
     }
 }
+    function Pemdas() {
+        for (let i = 0; i < operationArray.length; i++) {
+            switch (operationArray[i]) {
+                case '(':
+                    queue.push('a');
+                    break;
+                case '*':
+                    queue.push('ba');
+                    break;
+                case '/':
+                    queue.push('bb');
+                    break;
+                case '%':
+                    queue.push('ca');
+                    break;
+                case '+':
+                    queue.push('da');
+                    break;
+                case '-':
+                    queue.push('db');
+                    break;
+            }
+        }
+        Calculate()
+    }
